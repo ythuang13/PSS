b321478762e160e975040f7964e77822b5c1894b
ythuang13 <ythuang13@gmail.com>
2022-05-02 14:37:00 -0700

    implement main loop

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from pss import PSS
 
 def main():
+    pss = PSS()
+    pss.readFromFile(default=True)
+
+    running = True
+    while running:
+        option = input("Enter option: ")
+        if option == "1":
+            # manage task
+            manageTask(pss)
+        elif option == "2":
+            # manage schedule
+            manageSchedule(pss)
+        elif option == "3":
+            # quit
+            print("Goodbye!")
+            running = False
+        else:
+            print("Invalid option")
+
+    pss.writeToFile(default=True)
+
+def manageTask(pss: PSS) -> None:
     pass
+
+def manageSchedule(pss: PSS) -> None:
+    pass
+
 
 if __name__ == '__main__':
     main()
