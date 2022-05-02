690b20c9ca583a7ea2ee0761859d63f0a18851da
ythuang13 <ythuang13@gmail.com>
2022-05-02 15:22:36 -0700

    added comments

--- pss.py
+++ pss.py
@@ -4,43 +4,88 @@ class PSS:
         self._defaultFilePath = "data.json"
 
     def createTask(self) -> None:
+        """User select type of task, pss verify data including,
+        name, date, time and duration before create the task
+        and store in tasksList"""
         pass
 
     def findTask(self) -> None:
+        """User search task by name.
+        If found, display the task else display not found"""
         pass
 
     def deleteTask(self) -> None:
+        """User select task to delete using task name.
+        If found, validate if delete anti-task would create conflict,
+        also delete recurring task and its anti-task together"""
         pass
 
     def editTask(self) -> None:
+        """User select task to edit using task name.
+        If found, editor display the current attributes of the task
+        and ask user to edit the attributes."""
         pass
 
-    def writeToFile(self) -> None:
+    def writeToFile(self, default=False) -> None:
+        """Default is to write to default file path.
+        otherwise, ask user for file name, check validity,
+        and write schedule to the file using JSON format.
+        Default is to do all, but user can specify for day, week, month and its start date.
+        @param default: True if default file path is use, otherwise 
+            ask for user input
+        """
         pass
 
-    def readFromFile(self) -> None:
+    def readFromFile(self, default=False) -> None:
+        """Default is to read from default file path.
+        otherwise, ask user for file name, check validity,
+        and read schedule to the file using JSON format.
+        Need to check task information before store the data.
+        Default is to do all, but user can specify for day, week, month and its start date.
+        @param default: True if default file path is use, otherwise 
+        ask for user input
+        """
         pass
 
     def viewSchedule(self) -> None:
+        """User input the start date and the period for the schedule.
+        PSS sort and display all of the tasks, except for 
+        recurring that's cancel out with anti-task
+        """
         pass
 
     def fileVerification(self) -> bool:
+        """Check if the file is exists or not"""
         return True
 
     def jsonVerification(self) -> bool:
+        """Check if the file is in valid JSON format or not"""
         return True
 
     def taskVerification(self) -> bool:
+        """ Check whether the task is valid or not.
+        Time must be correct format, no conflic, name must be unique.
+        """
         return True
 
     def nameVerification(self) -> bool:
+        """Return True if name is uniqe, else False"""
         return True
 
-    def dateVerification(self) -> bool:
+    def dateVerification(self, type: str) -> bool:
+        """Check if the date is valid or not
+        Date format: YYYYMMDD
+        month range from [01-12], day range from [01-last day of the month]
+        end date must be later than start date.
+        @param type: recurring, transient or anti
+        """
         return True
 
     def generateSchedule() -> None:
+        """Generate schedule for a period of time in json format,
+        default is all"""
         pass
 
     def displaySchedule() -> None:
+        """Display the schedule, used for view schedule"""
         pass
