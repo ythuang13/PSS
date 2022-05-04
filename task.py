7413de1ecc5e0baa28b44d677db7df3fddc126f1
ythuang13 <ythuang13@gmail.com>
2022-05-04 14:57:30 -0700

    some assertion testing

--- task.py
+++ task.py
@@ -1,8 +1,14 @@
+from setting import ANTI_TASKS, FREQUENCIES, RECURRING_TASKS, TRANSIENT_TASKS
+
+
 class Task:
     def __init__(self, name: str, duration: float, startTime: float):
         self._name = name
         self._duration = duration
         self._start_time = startTime
+        assert type(self._name) == str
+        assert type(self._duration) == float
+        assert type(self._start_time) == float
     
     def getName(self) -> str:
         return self._name
@@ -24,6 +30,14 @@ class RecurringTask(Task):
         self._start_date = startDate
         self._end_date = endDate
         self._frequency = frequency
+
+        assert type(self._type) == str
+        assert type(self._start_date) == int
+        assert type(self._end_date) == int
+        assert type(self._frequency) == int
+        assert self._start_date <= self._end_date
+        assert self._frequency in FREQUENCIES
+        assert self._type in RECURRING_TASKS
     
     def getType(self) -> str:
         return self._type
@@ -46,6 +60,10 @@ class TransientTask(Task):
         super().__init__(name, duration, startTime)
         self._type = type
         self._date = date
+
+        assert type(self._type) == str
+        assert type(self._date) == int
+        assert self._type in TRANSIENT_TASKS
     
     def getType(self) -> str:
         return self._type
@@ -61,6 +79,9 @@ class AntiTask(Task):
             date: int):
         super().__init__(name, duration, startTime)
         self._date = date
+
+        assert type(self._date) == int
+        assert self._type in ANTI_TASKS
     
     def getDate(self) -> int:
         return self._date
