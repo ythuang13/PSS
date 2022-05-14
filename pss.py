1bc609b212a1e2357c22c6d45fdff48b44de8d83
Ryan Trinh <rqtrinh@cpp.edu>
2022-05-14 11:21:50 -0700

    Made small changes int Anti-Task verification

--- pss.py
+++ pss.py
@@ -783,8 +783,7 @@ class PSS:
                         other_end_time = other_start_time + other_task.getDuration()
                         
                         #check if the times of the class overlap
-                        if(not((task_start_time < other_start_time and task_end_time <= other_start_time)or
-                            (task_start_time >= other_end_time and task_end_time > other_end_time))):
+                        if(not(task_start_time == other_start_time and task_end_time == other_end_time)):
                             return False
 
                 #check AntiTask against TransientTask
